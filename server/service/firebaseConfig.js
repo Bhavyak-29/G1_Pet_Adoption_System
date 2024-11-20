1e41816bc85f4736ef12fe3c55ce2184a35243b2
Bhavyak-29 <202201029@daiict.ac.in>
2024-11-20 19:27:57 +0530

    Update firebaseConfig.js

--- server/service/firebaseConfig.js
+++ server/service/firebaseConfig.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('/etc/secrets/serviceAccountKey');
+const serviceAccount = require('/etc/secrets/serviceAccountKey.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
